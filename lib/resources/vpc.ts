42c04a6a6413b5978ec81a35afb8d425b31fa954
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-16 16:50:21 +0900

    refactor-2

--- lib/resources/vpc.ts
+++ lib/resources/vpc.ts
@@ -1,18 +1,18 @@
 import { CfnVPC } from 'aws-cdk-lib/lib/aws-ec2';
 import { Construct } from 'constructs';
+import { Resource } from './resource';
 
-export class Vpc {
+export class Vpc extends Resource {
   public vpc: CfnVPC;
 
-  constructor() {}
+  constructor() {
+    super();
+  }
 
   public createResources(scope: Construct) {
-    const systemName = scope.node.tryGetContext('systemName');
-    const envType = scope.node.tryGetContext('envType');
-
     this.vpc = new CfnVPC(scope, 'Vpc', {
       cidrBlock: '10.0.0.0/16',
-      tags: [{ key: 'Name', value: `${systemName}-${envType}-vpc` }],
+      tags: [{ key: 'Name', value: this.createResourceName(scope, 'vpc') }],
     });
   }
 }
