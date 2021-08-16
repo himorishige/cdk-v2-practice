287b19c91a3177e9c626181d8658193e61b120c4
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-16 15:52:09 +0900

    add: subnet

--- lib/cdk-practice-stack.ts
+++ lib/cdk-practice-stack.ts
@@ -1,20 +1,64 @@
 import { Construct } from 'constructs';
-import { App, Stack, StackProps } from 'aws-cdk-lib';
-import { CfnVPC } from 'aws-cdk-lib/lib/aws-ec2';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnSubnet, CfnVPC } from 'aws-cdk-lib/lib/aws-ec2';
 
 export class CdkPracticeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    new CfnVPC(this, 'Vpc', {
-      cidrBlock: '192.168.0.0/20',
+    const systemName = this.node.tryGetContext('systemName');
+    const envType = this.node.tryGetContext('envType');
+
+    const vpc = new CfnVPC(this, 'Vpc', {
+      cidrBlock: '10.0.0.0/16',
       tags: [
         {
           key: 'Name',
-          value: 'practice-dev-vpc',
+          value: `${systemName}-${envType}-vpc`,
         },
       ],
+    });
+
+    const subnetPublic1a = new CfnSubnet(this, 'SubnetPublic1a', {
+      cidrBlock: '10.0.11.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-public-1a` }],
+    });
+
+    const subnetPublic1c = new CfnSubnet(this, 'SubnetPublic1c', {
+      cidrBlock: '10.0.12.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-public-1c` }],
+    });
+
+    const subnetApp1a = new CfnSubnet(this, 'SubnetApp1a', {
+      cidrBlock: '10.0.21.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-app-1a` }],
+    });
+
+    const subnetApp1c = new CfnSubnet(this, 'SubnetApp1c', {
+      cidrBlock: '10.0.22.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-app-1c` }],
+    });
+
+    const subnetDb1a = new CfnSubnet(this, 'SubnetDb1a', {
+      cidrBlock: '10.0.31.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1a',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-db-1a` }],
+    });
+
+    const subnetDb1c = new CfnSubnet(this, 'SubnetDb1c', {
+      cidrBlock: '10.0.32.0/24',
+      vpcId: vpc.ref,
+      availabilityZone: 'ap-northeast-1c',
+      tags: [{ key: 'Name', value: `${systemName}-${envType}-subnet-db-1c` }],
     });
   }
 }
--- test/cdk-practice.test.ts
+++ test/cdk-practice.test.ts
@@ -2,7 +2,7 @@ import { expect as expectCDK, countResources, haveResource } from '@aws-cdk/asse
 import { App } from 'aws-cdk-lib';
 import * as CdkPractice from '../lib/cdk-practice-stack';
 
-test('Empty Stack', () => {
+test('Vpc', () => {
   const app = new App();
   // WHEN
   const stack = new CdkPractice.CdkPracticeStack(app, 'MyTestStack');
@@ -10,8 +10,58 @@ test('Empty Stack', () => {
   expectCDK(stack).to(countResources('AWS::EC2::VPC', 1));
   expectCDK(stack).to(
     haveResource('AWS::EC2::VPC', {
-      CidrBlock: '192.168.0.0/20',
-      Tags: [{ Key: 'Name', Value: 'practice-dev-vpc' }],
+      CidrBlock: '10.0.0.0/16',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-vpc' }],
+    }),
+  );
+});
+
+test('Subnet', () => {
+  const app = new App();
+  // WHEN
+  const stack = new CdkPractice.CdkPracticeStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(countResources('AWS::EC2::Subnet', 6));
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.11.0/24',
+      AvailabilityZone: 'ap-northeast-1a',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-public-1a' }],
+    }),
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.12.0/24',
+      AvailabilityZone: 'ap-northeast-1c',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-public-1c' }],
+    }),
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.21.0/24',
+      AvailabilityZone: 'ap-northeast-1a',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-app-1a' }],
+    }),
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.22.0/24',
+      AvailabilityZone: 'ap-northeast-1c',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-app-1c' }],
+    }),
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.31.0/24',
+      AvailabilityZone: 'ap-northeast-1a',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-db-1a' }],
+    }),
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::EC2::Subnet', {
+      CidrBlock: '10.0.32.0/24',
+      AvailabilityZone: 'ap-northeast-1c',
+      Tags: [{ Key: 'Name', Value: 'undefined-undefined-subnet-db-1c' }],
     }),
   );
 });
