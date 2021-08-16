27a0003bd4857357d01aed1eeade8b31618c6c6b
Hiroshi MORISHIGE <hiroshi.morishige@gmail.com>
2021-08-16 14:33:39 +0900

    initial commit

--- lib/cdk-practice-stack.ts
+++ lib/cdk-practice-stack.ts
@@ -1,9 +1,20 @@
-import * as cdk from '@aws-cdk/core';
+import { Construct } from 'constructs';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnVPC } from 'aws-cdk-lib/lib/aws-ec2';
 
-export class CdkPracticeStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class CdkPracticeStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new CfnVPC(this, 'Vpc', {
+      cidrBlock: '192.168.0.0/20',
+      tags: [
+        {
+          key: 'Name',
+          value: 'practice-dev-vpc',
+        },
+      ],
+    });
   }
 }
--- test/cdk-practice.test.ts
+++ test/cdk-practice.test.ts
@@ -1,13 +1,18 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
+import { App } from 'aws-cdk-lib';
 import * as CdkPractice from '../lib/cdk-practice-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkPractice.CdkPracticeStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new App();
+  // WHEN
+  const stack = new CdkPractice.CdkPracticeStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT,
+    ),
+  );
 });
